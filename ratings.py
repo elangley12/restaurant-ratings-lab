f4a8690d67226e79b901ee0b07b282d8f3ce8ae6
Emily <emily.j.langley@gmail.com>
2024-03-04 16:33:02 -0800

    changed dictionary creation method

--- ratings.py
+++ ratings.py
@@ -1,18 +1,20 @@
 def rate_restaurant(ratings):
     """Restaurant rating lister."""
 
-    text_file = open(ratings)
-    abc_scores = {}
-
-    for line in text_file:                                              # parse through each line of the text file
-        line = line.rstrip().split(":")                                 # remove each word, one line at a time
-        abc_scores = sorted(abc_scores[line])
-        # for word in words:                                            # loop through text file word by word
-        #     abc_scores[word] = abc_scores.get(word, 0) + 1            # add one for each word to new dictionary
+    scores = open("scores.txt")
+    r_scores = {}
+    for line in scores:
+        line = line.rstrip()
+        restaurant, score = line.split(":")
+        r_scores[restaurant] = int(score)
     
-        for key, value in abc_scores.items():                           # loop over each key value pair in word_counts
-            print(f"{key} {value}")                                     # print each key value pair on separate lines
+    abc_scores = sorted(r_scores)
+    
+    for key, value in abc_scores.items():                           # loop over each key value pair in word_counts
+        print(f"{key} is rated at {value}.")                                     # print each key value pair on separate lines
 
-    # abc_scores = sorted(abc_scores)
+    return abc_scores
 
-    # return abc_scores
+
+
+    # return r_scores
