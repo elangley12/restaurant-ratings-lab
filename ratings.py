4cd9d716b0402b0e5697e110015acb0e0ce8ce3d
Emily <emily.j.langley@gmail.com>
2024-03-04 17:09:58 -0800

    completed rate_restaurant(r_scores) and add_restaurant()

--- ratings.py
+++ ratings.py
@@ -1,20 +1,30 @@
-def rate_restaurant(ratings):
+def rate_restaurant(r_scores):
     """Restaurant rating lister."""
 
     scores = open("scores.txt")
-    r_scores = {}
+    # r_scores = {}
     for line in scores:
         line = line.rstrip()
         restaurant, score = line.split(":")
         r_scores[restaurant] = int(score)
+        
+
+    abc_scores = dict(sorted(r_scores.items()))
     
-    abc_scores = sorted(r_scores)
+    for key, value in abc_scores.items():
+        print(f"{key} is rated at {value}.")
     
-    for key, value in abc_scores.items():                           # loop over each key value pair in word_counts
-        print(f"{key} is rated at {value}.")                                     # print each key value pair on separate lines
+    
+def add_restaurant():
+    """Asks the user to add one new restaurant to scores.txt"""
 
-    return abc_scores
+    print("Add your favourite restuarant!")
+    name = input("Restuarant name: ")
+    scores = input("Rating: ")
+    r_scores = {}
+
+    r_scores[name] = scores
+    rate_restaurant(r_scores)
 
 
-
-    # return r_scores
+add_restaurant()
